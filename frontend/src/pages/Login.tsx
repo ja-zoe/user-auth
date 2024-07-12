4d6cee5c690c2629dde9223b67c31bf3d9b77b85
Julian Vilfort <j.vilfort01@gmail.com>
2024-07-12 01:12:58 -0400

    Delete users variable and useEffect in login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,8 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
 
 const Login = () => {
-  const [users, setUsers] = useState([])
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const navigate = useNavigate()
